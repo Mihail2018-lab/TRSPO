4859ac8f3440a4cd421e52865fcfd394a77085be
Mihail2018-lab <79443320+Mihail2018-lab@users.noreply.github.com>
2021-11-03 14:26:03 +0200

    Update Работа с потоками.cs

--- Работа с потоками.cs
+++ Работа с потоками.cs
@@ -1,158 +1,3 @@
-﻿using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-
-//namespace Lab2_TRSPO
-//{
-//    class Program
-//    {
-//        private static AutoResetEvent waitHandle = new AutoResetEvent(false);
-
-//        static void Main(string[] args)
-//        {
-
-//            Stopwatch stopwatch = new Stopwatch();
-
-//            // Begin timing.
-//            //stopwatch.Start();
-
-
-//            int[,] array;
-//            Console.WriteLine("Columns:");
-//            int columns = Convert.ToInt32(Console.ReadLine());
-//            Console.WriteLine("Rows:");
-//            int rows = Convert.ToInt32(Console.ReadLine());
-//            array = new int[rows, columns];
-//            Console.WriteLine("Number of thread:");
-//            int number_thread = Convert.ToInt32(Console.ReadLine());
-//            Random random_array = new Random();
-
-
-//            StreamWriter sr = new StreamWriter("test1.txt");
-//            //StreamWriter sr1 = new StreamWriter("result1.txt");
-//            Thread[] Array_thread = new Thread[number_thread];
-
-
-//            Console.WriteLine("Start array:");
-//            for (int i = 0; i < rows; i++)
-//            {
-//                for (int j = 0; j < columns; j++)
-//                {
-//                    array[i, j] = random_array.Next(-100, 100);
-//                    //Console.Write("{0}\t", array[i, j]);
-//                    // запись в файл
-//                    sr.Write(array[i, j].ToString() + " ");
-//                }
-//               sr.WriteLine();
-//               //Console.WriteLine();
-//            }
-//            sr.Close();
-//            StreamWriter sr1 = new StreamWriter("result1.txt");
-//            Sort[] sortarray = new Sort[columns];
-//            int y = 0;
-//            //sortarray.columns = 0;
-//            //sortarray.array = array;
-//            //sortarray.rows = rows;
-
-//            Console.WriteLine("Sort array:");
-//            stopwatch.Start();
-
-
-//            int f = 0;
-
-
-//           Queue<Sort> queue = new Queue<Sort>();
-//            for (int i = 0; i < columns; i++)
-//            {
-//                sortarray[i] = new Sort();
-//                sortarray[i].columns = y;
-//                sortarray[i].array = array;
-//                sortarray[i].rows = rows;
-//                sortarray[i].sr1 = sr1;
-//                queue.Enqueue(sortarray[i]);
-//                y++;
-//            }
-
-
-
-//            while (true)
-//            {
-//                if(f >= (number_thread-1))
-//                {
-//                    f = 0;
-//                }
-
-//                if (queue.Count != 0)
-//                {
-//                    //Array_thread[f] = new Thread(new ParameterizedThreadStart(sortingarray));
-//                    //Array_thread[f].IsBackground = true;
-//                    //Array_thread[f].Start(sortarray);
-//                    Array_thread[f] = new Thread(sortingarray);
-//                    Array_thread[f].IsBackground = true;
-//                    Array_thread[f].Start(queue);
-//                }
-//                else
-//                    break;
-
-//                waitHandle.WaitOne();
-//                //sortarray.columns += 1;
-//                f+=1;
-//                //Console.WriteLine("Все потоки завершились");
-//            }
-//            sr1.Close();
-
-
-//            stopwatch.Stop();
-
-//            // Write result.
-//            Console.WriteLine("Time elapsed: {0}", stopwatch.Elapsed);
-
-//            Console.ReadKey();
-//        }
-
-
-
-//        public static void sortingarray(object obj)
-//        {
-//            Queue<Sort> queue1 = (Queue<Sort>)obj;
-//            Sort sort_array = (Sort)queue1.Dequeue();
-//            //Sort sort_array = (Sort)obj;
-//            int key = 0, f = 0;
-
-//            for (int m = 1; m < sort_array.rows; m++)
-//            {
-//                key = sort_array.array[m, sort_array.columns];
-//                f = m - 1;
-//                while (f >= 0 && sort_array.array[f, sort_array.columns] > key)
-//                {
-//                    sort_array.array[f + 1, sort_array.columns] = sort_array.array[f, sort_array.columns];
-//                    f--;
-//                }
-//                sort_array.array[f + 1, sort_array.columns] = key;
-//            }
-//            //Console.Write("Sort array:\n");
-//            for (int j = 0; j < sort_array.rows; j++)
-//            {
-//                sort_array.sr1.Write(sort_array.array[j, sort_array.columns].ToString() + " ");
-//                //Console.Write("{0}\t", sort_array.array[j, sort_array.columns]);
-//            }
-//            //Console.WriteLine();
-//            sort_array.sr1.WriteLine();
-//            waitHandle.Set();
-//        }
-
-//    }
-//}
-
-
-
-
 using System;
 using System.Threading;
 using System.Collections.Concurrent;
@@ -240,11 +85,11 @@ namespace Lab2_TRSPO
             //Console.Write("Sort array:\n");
             for (int j = 0; j < sort_array.rows; j++)
             {
-                sort_array.sr1.WriteAsync(sort_array.array.ToString() + " ");
+                sort_array.sr1.Write(sort_array.array[j, sort_array.columns].ToString() + " ");
                 //Console.Write("{0}\t", sort_array.array[j, sort_array.columns]);
             }
             //Console.WriteLine();
-            sort_array.sr1.WriteLineAsync();
+            sort_array.sr1.WriteLine();
             Thread.Sleep(200);
             //waitHandle.Set();
         }
@@ -281,7 +126,7 @@ namespace Lab2_TRSPO
                 //Console.WriteLine();
             }
             sr.Close();
-            StreamWriter sr1 = new StreamWriter("result1.txt", true);
+            StreamWriter sr1 = new StreamWriter("result1.txt");
 
             ConcurrentQueue<TaskItem> queue = new ConcurrentQueue<TaskItem>();
             Thread[] threads = new Thread[number_thread];
@@ -314,4 +159,3 @@ namespace Lab2_TRSPO
         }
     }
 }
-
